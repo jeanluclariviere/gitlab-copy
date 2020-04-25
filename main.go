aa04e013521776dcc24ea54caad504aca2eb633e
jeanluc <jeanluc@debian>
2020-04-25 18:21:11 -0400

    testing group creation

--- main.go
+++ main.go
@@ -2,8 +2,43 @@
 
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const u = "http://192.168.0.16:4080"
+const t = "5eoo8jg3aiwyDJN3Z9g8"
+
 func main() {
 	//setup()
 	//fetchCredentials()
-	login()
+	//login()
+	client := http.Client{}
+	params := map[string]string{
+		"name":      "hello",
+		"path":      "hello",
+		"parent_id": "2",
+	}
+
+	req := newGroup(u, params)
+	req.Header.Add("PRIVATE-TOKEN", t)
+	resp, _ := client.Do(req)
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
+}
+
+func migrate(pid, dst string) {
+
+	//Create subgroups
+	//split string
+
+	/* 	params := map[string]string{
+		"namespace": "sample",
+		"path":      "upload-test",
+		"overwrite": "false",
+	} */
 }
