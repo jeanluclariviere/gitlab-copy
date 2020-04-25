46969a7d1e8add763310e19960b4560058eee2f2
jeanluc <jeanluc@debian>
2020-04-25 18:20:49 -0400

    Adding groups

--- gitlab.go
+++ gitlab.go
@@ -1,21 +1,70 @@
+//{"message":"404 Namespace Not Found"}
+
 package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
+	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
 const projects = "/api/v4/projects/"
+const groups = "/api/v4/groups/"
+
+func newGroup(uri string, params map[string]string) *http.Request {
+	uri = uri + groups
+
+	data := url.Values{}
+	for k, v := range params {
+		data.Add(k, v)
+	}
+
+	req, err := http.NewRequest("POST", uri, bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return req
+}
+
+func searchGroup(uri string, name string) string {
+	uri = uri + groups + "?search=" + name
+
+	resp, err := http.Post("POST", uri, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := struct {
+		ID          int
+		name        string
+		path        string
+		description string
+	}{}
+
+	if err := json.Unmarshal(bs, &r); err != nil {
+		log.Fatal(err)
+	}
+
+	return strconv.Itoa(r.ID)
+}
 
 func scheduleExport(uri, pid string) *http.Request {
-
-	url := uri + projects + pid + "/export/"
-	req, err := http.NewRequest("POST", url, nil)
+	uri = uri + projects + pid + "/export/"
+	req, err := http.NewRequest("POST", uri, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,8 +73,8 @@ func scheduleExport(uri, pid string) *http.Request {
 }
 
 func exportStatus(uri, pid string) *http.Request {
-	url := uri + projects + pid + "/export/"
-	req, err := http.NewRequest("GET", url, nil)
+	uri = uri + projects + pid + "/export/"
+	req, err := http.NewRequest("GET", uri, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,9 +83,8 @@ func exportStatus(uri, pid string) *http.Request {
 }
 
 func exportDownload(uri, pid string) *http.Request {
-
-	url := uri + projects + pid + "/export/download"
-	req, err := http.NewRequest("GET", url, nil)
+	uri = uri + projects + pid + "/export/download"
+	req, err := http.NewRequest("GET", uri, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,9 +92,8 @@ func exportDownload(uri, pid string) *http.Request {
 	return req
 }
 
-func importFile(uri string, params map[string]string, paramName, path string) *http.Request {
-
-	url := uri + projects + "/import"
+func importFile(uri string, params map[string]string, path string) *http.Request {
+	uri = uri + projects + "/import"
 
 	// Open a file
 	file, err := os.Open(path)
@@ -66,7 +113,7 @@ func importFile(uri string, params map[string]string, paramName, path string) *h
 
 	// Create a new form-data header with the provided field name and file name
 	// And returns an io.writer
-	part, err := writer.CreateFormFile(paramName, filepath.Base(path))
+	part, err := writer.CreateFormFile("file", filepath.Base(path))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -88,7 +135,7 @@ func importFile(uri string, params map[string]string, paramName, path string) *h
 	}
 
 	// Create a new request and return it
-	req, err := http.NewRequest("POST", url, body)
+	req, err := http.NewRequest("POST", uri, body)
 	// You must set the content type
 	req.Header.Set("Content-Type", writer.FormDataContentType())
 	return req
