089fb6940b9143be65a8da316b09e433564c7b16
jeanluc <jeanluc@debian>
2020-04-26 12:37:45 -0400

    Creating migrate function

--- main.go
+++ main.go
@@ -2,7 +2,11 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
 const u = "http://192.168.0.16:4080"
 const t = "5eoo8jg3aiwyDJN3Z9g8"
@@ -11,21 +15,33 @@ func main() {
 	//setup()
 	c := fetchCredentials()
 	//login()
-
-	fmt.Println("GroupID:", newGroup(c.ExportURI, c.ExportToken, "bird/bird"))
-
-	//fmt.Println(getParentID(c.ExportURI, c.ExportToken, "test", "hello/test"))
+	newGroup(c.ExportURI, c.ExportToken, "hello/test2")
+	fmt.Println(getParentID(c.ExportURI, c.ExportToken, "test2", "hello/test2"))
 
 }
 
 func migrate(pid, dst string) {
 
-	//Create subgroups
-	//split string
+	c := login()
+	_, err := scheduleExport(c.ExportURI, c.ExportToken, pid)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	/* 	params := map[string]string{
-		"namespace": "sample",
-		"path":      "upload-test",
-		"overwrite": "false",
-	} */
+	for retry := 0; retry < 5; retry++ {
+		r, _, err := exportStatus(c.ExportURI, c.ExportToken, pid)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if r.ExportStatus == "finished" {
+
+			t := time.Now()
+			filename := "./" + t.Format("01-02-2006") + "-" + r.Path + "tar.gz"
+			exportDownload(c.ExportURI, c.ExportToken, pid, filename)
+			break
+		}
+
+		time.Sleep(10 * time.Second)
+	}
 }
